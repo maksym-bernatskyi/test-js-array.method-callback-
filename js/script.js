fb553d20c059aaaa69065a9f63fd347c536a80c6
Maksym Bernatskyi <bernasik007@gmail.com>
2021-12-15 22:16:53 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// console.log(_.isEmpty({}));
+// console.log(_.isEmpty({ a: 1 }));
+
+
+// const user = {
+//     name: 'mango',
+//     location: {
+//         coords: [1, 2],
+//         city: 'Lviv',
+//     },
+// };
+
+// console.log(_.get(user, 'location.city'))
+
+// //** Новый метод поиска по вложенности */
+// const user = {
+//     name: 'mango',
+//     location: {
+//         coords: [1, 2],
+//         city: 'Lviv',
+//     },
+// };
+
+// console.log(user?.location?.city); // <--
+
+
+//** Объединение уникальных элементов 2 массивов */
+// console.log(_.union([1, 2, 3], [3, 4, 5]));
+
+
+// const users = [
+//     { user: 'fred', age: 48 },
+//     { user: 'barney', age: 36 },
+//     { user: 'fred', age: 40 },
+//     { user: 'barney', age: 34 },
+// ];
+
+// console.log(_.sortBy(users, user => user.age));
+
+
+// console.log(_.sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+
